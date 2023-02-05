8e87fe5ebb9f9f2e9c6fa4da726c23985c626d98
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-02-04 20:06:06 -0900

    docs

--- scaffold/render_funcs.go
+++ scaffold/render_funcs.go
@@ -16,6 +16,8 @@ type RenderableNode interface {
 	io.ReadWriter
 }
 
+// RenderNode renders a node and sets the outpath on the node while
+// processing it as a template.
 func RenderNode[T RenderableNode](s *Engine, n T, vars any) error {
 	outpath, err := s.TmplString(n.GetTemplatePath(), vars)
 	if err != nil {
@@ -32,6 +34,10 @@ func RenderNode[T RenderableNode](s *Engine, n T, vars any) error {
 	return nil
 }
 
+// RenderReadWriter renders a io.ReadWriter as a template.
+//
+//   - The io.Reader is expected to be a template.
+//   - The io.Writer is expected to be a destination for the rendered template. (e.g. a file)
 func RenderReadWriter[T io.ReadWriter](s *Engine, rw T, vars any) error {
 	tmpl, err := s.TmplFactory(rw)
 	if err != nil {
@@ -46,10 +52,27 @@ func RenderReadWriter[T io.ReadWriter](s *Engine, rw T, vars any) error {
 	return nil
 }
 
+// ProjectRenderOptions are options for rendering a project.
 type ProjectRenderOptions struct {
+	// OutDirectory is the directory to render the project to.
+	//
+	// Example: /home/user/projects
+	//
+	//  | /home/user/projects/my-project
+	//	|___ <- New project rendered here
 	OutDirectory string
 }
 
+// RenderProject renders an entire Project to the given out directory.
+//
+// It will also transforms the vars variable to be used in the templates
+// into a nested map of the following structure:
+//
+//   - Project: The project name
+//   - ProjectSnake: The project name in snake case
+//   - ProjectKebab: The project name in kebab case
+//   - ProjectCamel: The project name in camel case
+//   - Scaffold: The vars variable passed to this function
 func RenderProject(e *Engine, p *Project, vars any, opts ProjectRenderOptions) error {
 	iVars := Vars{
 		"Project":      p.qProject,
@@ -74,11 +97,7 @@ func RenderProject(e *Engine, p *Project, vars any, opts ProjectRenderOptions) e
 	}
 
 	for _, node := range nodes {
-		log.Debug().Str("path", node.GetTemplatePath()).Msg("Rendering node")
-
 		if node.folder {
-			log.Debug().Str("path", node.GetTemplatePath()).Msg("Creating folder")
-
 			path := node.GetTemplatePath()
 			path, err := e.TmplString(path, iVars)
 			if err != nil {
@@ -87,7 +106,6 @@ func RenderProject(e *Engine, p *Project, vars any, opts ProjectRenderOptions) e
 
 			path = filepath.Join(projectPath, path)
 
-			log.Debug().Str("path", path).Msg("Creating folder")
 			err = os.Mkdir(path, 0755)
 			if err != nil && !os.IsExist(err) {
 				return err
@@ -103,15 +121,15 @@ func RenderProject(e *Engine, p *Project, vars any, opts ProjectRenderOptions) e
 
 		err = RenderNode(e, node, iVars)
 		if err != nil {
+			close()
 			return fmt.Errorf("failed to render node: %w", err)
 		}
 
 		outpath := filepath.Join(projectPath, node.outpath)
 
-		// Write to file
-		log.Debug().Str("path", outpath).Msg("Writing to file")
 		err = os.WriteFile(outpath, node.outContent, 0644)
 		if err != nil {
+			close()
 			return fmt.Errorf("failed to write to file: %w", err)
 		}
 

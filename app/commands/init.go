1affd19b62dbf4029e05837622086772a9b5d2c0
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-05 14:40:50 -0900

    new init command

---
+++ app/commands/init.go
@@ -0,0 +1,85 @@
+package commands
+
+import (
+	"embed"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bmatcuk/doublestar/v4"
+	"github.com/urfave/cli/v2"
+)
+
+//go:embed init/*
+var initFiles embed.FS
+
+func (ctrl *Controller) Init(ctx *cli.Context) error {
+	// get current directory
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	scaffoldDir := filepath.Join(dir, ".scaffold")
+
+	// Check if exists
+	if _, err := os.Stat(scaffoldDir); !os.IsNotExist(err) {
+		return err
+	}
+
+	// Create directory
+	if err := os.Mkdir(scaffoldDir, 0755); err != nil {
+		return err
+	}
+
+	// Write files from initFiles embed.FS to .scaffold
+	// Write files from initFiles embed.FS to .scaffold
+	files, err := doublestar.Glob(initFiles, "init/**/*")
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		f, err := initFiles.Open(file)
+		if err != nil {
+			return err
+		}
+
+		// Check if dir
+		finfo, err := f.Stat()
+		if err != nil {
+			return err
+		}
+
+		fileName := strings.TrimPrefix(file, "init/")
+
+		if finfo.IsDir() {
+			err := os.Mkdir(filepath.Join(scaffoldDir, fileName), 0755)
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		out, err := os.Create(filepath.Join(scaffoldDir, fileName))
+		if err != nil {
+			return err
+		}
+
+		if _, err := io.Copy(out, f); err != nil {
+			return err
+		}
+
+		if err := out.Close(); err != nil {
+			return err
+		}
+
+		if err := f.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

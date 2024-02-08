6dbf24cdc10a6b7c3ebe988a3cf9557ffbdc7702
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-02-08 17:53:38 -0600

    expand test cases

--- app/scaffold/pkgs/resolver_test.go
+++ app/scaffold/pkgs/resolver_test.go
@@ -11,14 +11,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var noopAuthProvider = AuthProviderFunc(func(pkgurl string) (auth transport.AuthMethod, ok bool) {
+	return nil, false
+})
+
 // TestResolver_Resolve_Remote tests the Resolve method of the Resolver type with a remote
 // url passed as an argument. We use a temporary directory to store the cloned repository.
 // and override the Cloner with a ClonerFunc that creates the directory and returns the path.
-//
-// TODO: Update to test auth provider is called
 func TestResolver_Resolve_Remote(t *testing.T) {
 	tempdir := t.TempDir()
-
 	tempcache := filepath.Join(tempdir, "cache")
 	packagepath := filepath.Join(tempcache, "github.com", "hay-kot", "scaffold-go-cli")
 
@@ -33,14 +34,10 @@ func TestResolver_Resolve_Remote(t *testing.T) {
 
 	resolver := NewResolver(nil, tempcache, tempdir, WithCloner(clonefn))
 
-	checkDirs := []string{tempdir}
-
 	path, err := resolver.Resolve(
 		"https://github.com/hay-kot/scaffold-go-cli",
-		checkDirs,
-		AuthProviderFunc(func(pkgurl string) (auth transport.AuthMethod, ok bool) {
-			return nil, false
-		}),
+		nil,
+		noopAuthProvider,
 	)
 
 	require.NoError(t, err)
@@ -48,11 +45,66 @@ func TestResolver_Resolve_Remote(t *testing.T) {
 	assert.Equal(t, packagepath, path)
 }
 
-func TestResolver_Resolve_Absolute(t *testing.T) {
-}
+func TestResolver_Resolve_FilePaths(t *testing.T) {
+	tempdir := t.TempDir()
+	tempcwd := filepath.Join(tempdir, "cwd")
+	tempcache := filepath.Join(tempdir, "cache")
+	packagepath := filepath.Join(tempcwd, "scaffold", "test")
 
-func TestResolver_Resolve_Relative(t *testing.T) {
-}
+	// Setup .scaffold directory
+	{
+		err := os.MkdirAll(filepath.Join(tempcwd, ".scaffold", "new"), 0755)
+		require.NoError(t, err)
 
-func TestResolver_Resolve_Cwd(t *testing.T) {
+		// Create new/scaffold.yml file and new/templates dir
+
+		f, err := os.Create(filepath.Join(tempcwd, ".scaffold", "new", "scaffold.yml"))
+		require.NoError(t, err)
+		_, _ = f.Write([]byte("---"))
+		_ = f.Close()
+
+		err = os.MkdirAll(filepath.Join(tempcwd, ".scaffold", "new", "templates"), 0755)
+		require.NoError(t, err)
+	}
+
+	err := os.MkdirAll(packagepath, 0755)
+	require.NoError(t, err)
+
+	resolver := NewResolver(nil, tempcache, tempcwd)
+
+	testcases := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "relative path",
+			input:    "scaffold/test",
+			expected: packagepath,
+		},
+		{
+			name:     "absolute path",
+			input:    packagepath,
+			expected: packagepath,
+		},
+		{
+			name:     "local .scaffold directory",
+			input:    "new",
+			expected: filepath.Join(tempcwd, ".scaffold", "new"),
+		},
+	}
+
+	checkDirs := []string{
+		filepath.Join(tempcwd, ".scaffold"),
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			path, err := resolver.Resolve(tc.input, checkDirs, noopAuthProvider)
+
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.expected, path)
+		})
+	}
 }

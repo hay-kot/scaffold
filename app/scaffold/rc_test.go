39c4d9e565fc6b34b336ec9ca23764677f2fa8e3
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-02-08 09:17:39 -0600

    RC tests for authentication

--- app/scaffold/rc_test.go
+++ app/scaffold/rc_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"testing"
 
+	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -56,4 +57,90 @@ func TestNewScaffoldRC(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestScaffoldRC_Authenticator(t *testing.T) {
+	authscaffoldRC := []byte(`---
+auth:
+  - match: "^https://github.com"
+    basic:
+      username: user
+      password: pass
+  - match: "^https://gitea.com"
+    token: token-value
+`)
+
+	rc, err := NewScaffoldRC(bytes.NewReader(authscaffoldRC))
+	require.NoError(t, err)
+
+	auth, ok := rc.Authenticator("https://github.com/hay-kot/scaffold-go")
+	require.True(t, ok)
+
+	basic, ok := auth.(*githttp.BasicAuth)
+	require.True(t, ok)
+
+	assert.Equal(t, "user", basic.Username)
+	assert.Equal(t, "pass", basic.Password)
+
+	// Token Auth
+	auth, ok = rc.Authenticator("https://gitea.com/hay-kot/scaffold-go")
+	require.True(t, ok)
+
+	token, ok := auth.(*githttp.TokenAuth)
+	require.True(t, ok)
+
+	assert.Equal(t, "token-value", token.Token)
+
+	// No Auth
+	auth, ok = rc.Authenticator("https://gitlab.com/hay-kot/scaffold-go")
+	require.False(t, ok)
+	assert.Nil(t, auth)
+}
+
+func TestScaffoldRC_Authenticator_EnvExpansion(t *testing.T) {
+	const (
+		ScaffoldGithubUser = "scaffold-gh-user"
+		ScaffoldGithubPass = "scaffold-gh-pass"
+		ScaffoldGiteaToken = "scaffold-gitea-token"
+	)
+
+	t.Setenv("GITHUB_USER", ScaffoldGithubUser)
+	t.Setenv("GITHUB_PASS", ScaffoldGithubPass)
+	t.Setenv("GITEA_AUTH_TOKEN", ScaffoldGiteaToken)
+
+	authscaffoldRC := []byte(`---
+auth:
+  - match: "^https://github.com"
+    basic:
+      username: ${GITHUB_USER}
+      password: ${GITHUB_PASS}
+  - match: "^https://gitea.com"
+    token: ${GITEA_AUTH_TOKEN}
+`)
+
+	rc, err := NewScaffoldRC(bytes.NewReader(authscaffoldRC))
+	require.NoError(t, err)
+
+	auth, ok := rc.Authenticator("https://github.com/hay-kot/scaffold-go")
+	require.True(t, ok)
+
+	basic, ok := auth.(*githttp.BasicAuth)
+	require.True(t, ok)
+
+	assert.Equal(t, ScaffoldGithubUser, basic.Username)
+	assert.Equal(t, ScaffoldGithubPass, basic.Password)
+
+	// Token Auth
+	auth, ok = rc.Authenticator("https://gitea.com/hay-kot/scaffold-go")
+	require.True(t, ok)
+
+	token, ok := auth.(*githttp.TokenAuth)
+	require.True(t, ok)
+
+	assert.Equal(t, ScaffoldGiteaToken, token.Token)
+
+	// No Auth
+	auth, ok = rc.Authenticator("https://gitlab.com/hay-kot/scaffold-go")
+	require.False(t, ok)
+	assert.Nil(t, auth)
 }

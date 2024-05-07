3fdc12d0fe7015ae6b7880955fc3638de4e5b6bd
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-05-06 19:37:34 -0500

    simplify implementation

--- .examples/cli/{{ .ProjectKebab }}/main.go
+++ .examples/cli/{{ .ProjectKebab }}/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
@@ -26,52 +25,18 @@ func build() string {
 }
 
 func main() {
-	ctrl := &controller{}
-
 	app := &cli.App{
 		Name:    "{{ .Project }}",
 		Usage:   "{{ .Scaffold.description }}",
 		Version: build(),
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "cwd",
-				Usage: "current working directory",
-				Value: ".",
-			},
-			&cli.StringFlag{
-				Name:  "log-level",
-				Usage: "log level (debug, info, warn, error, fatal, panic)",
-				Value: "panic",
-			},
-		},
-		Before: func(ctx *cli.Context) error {
-			log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
-			ctrl.cwd = ctx.String("cwd")
-			ctrl.logLevel = ctx.String("log-level")
-
-			switch ctrl.logLevel {
-			case "debug":
-				log.Level(zerolog.DebugLevel)
-			case "info":
-				log.Level(zerolog.InfoLevel)
-			case "warn":
-				log.Level(zerolog.WarnLevel)
-			case "error":
-				log.Level(zerolog.ErrorLevel)
-			case "fatal":
-				log.Level(zerolog.FatalLevel)
-			default:
-				log.Level(zerolog.PanicLevel)
-			}
-
-			return nil
-		},
 		Commands: []*cli.Command{
 			{
-				Name:   "hello",
-				Usage:  "Says hello world",
-				Action: ctrl.HelloWorld,
+				Name:  "hello",
+				Usage: "Says hello world",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println("Hello, your favorite colors are {{ .Scaffold.colors | join `, ` }}")
+					return nil
+				},
 			},
 		},
 	}
@@ -79,14 +44,4 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal().Err(err).Msg("failed to run scaffold")
 	}
-}
-
-type controller struct {
-	cwd      string
-	logLevel string
-}
-
-func (c *controller) HelloWorld(ctx *cli.Context) error {
-	fmt.Println("Hello, your favorite colors are {{ .Scaffold.colors | join `, ` }}")
-	return nil
 }

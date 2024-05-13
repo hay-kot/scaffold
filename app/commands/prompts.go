850c66f5541bfa2778263c540334c791e147d0ec
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-05-13 12:49:05 -0500

    feat: nicer form for did you mean (#145)
    * use huh.Confirm for did you mean prompt
    
    * cleanup implementation
    
    * formatting

--- app/commands/prompts.go
+++ app/commands/prompts.go
@@ -1,16 +1,7 @@
 package commands
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/charmbracelet/huh"
-	"github.com/charmbracelet/lipgloss"
-)
-
-var (
-	bold     = lipgloss.NewStyle().Bold(true)
-	colorRed = lipgloss.NewStyle().Foreground(lipgloss.Color("#dc2626"))
 )
 
 func httpAuthPrompt() (username string, password string, err error) {
@@ -37,34 +28,16 @@ func httpAuthPrompt() (username string, password string, err error) {
 }
 
 func didYouMeanPrompt(given, suggestion string) bool {
-	bldr := strings.Builder{}
+	ok := true
 
-	// Couldn't find a scaffold named:
-	//   'foo'
-	//
-	// Did you mean:
-	//   'bar'?
-	//
-	// [y/n]:
+	err := huh.NewConfirm().
+		Title("Did You Mean " + suggestion + "?").
+		Description("Couldn't find '" + given + "'").
+		Value(&ok).
+		Run()
+	if err != nil {
+		return false
+	}
 
-	bldr.WriteString("\n ")
-	bldr.WriteString(bold.Render(colorRed.Render("could not find a scaffold named")))
-	bldr.WriteString("\n    ")
-	bldr.WriteString(given)
-	bldr.WriteString("\n\n")
-	bldr.WriteString(" ")
-	bldr.WriteString(bold.Render("did you mean"))
-	bldr.WriteString("\n    ")
-	bldr.WriteString(suggestion)
-	bldr.WriteString("?\n\n ")
-	bldr.WriteString("[y/n]: ")
-
-	out := bldr.String()
-
-	var resp string
-
-	fmt.Print(out)
-	fmt.Scanln(&resp)
-
-	return resp == "y"
+	return ok
 }
